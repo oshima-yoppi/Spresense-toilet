fd750eadc84880d5fcfe3fa511e38ba7fa0f3cf8
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-09-05 21:57:07 +0900

    a

--- src/AI/train.py
+++ src/AI/train.py
@@ -210,3 +210,27 @@ print(f"float model size: {float_model_size} MB")
 open("model.tflite", "wb").write(tflite_model)
 
 # %%
+import binascii
+
+
+def convert_to_c_array(bytes) -> str:
+    hexstr = binascii.hexlify(bytes).decode("UTF-8")
+    hexstr = hexstr.upper()
+    array = ["0x" + hexstr[i : i + 2] for i in range(0, len(hexstr), 2)]
+    array = [array[i : i + 10] for i in range(0, len(array), 10)]
+    return ",\n  ".join([", ".join(e) for e in array])
+
+
+tflite_binary = open("model.tflite", "rb").read()
+ascii_bytes = convert_to_c_array(tflite_binary)
+header_file = (
+    "const unsigned char model_tflite[] = {\n  "
+    + ascii_bytes
+    + "\n};\nunsigned int model_tflite_len = "
+    + str(len(tflite_binary))
+    + ";"
+)
+# print(c_file)
+open("model_data.h", "w").write(header_file)
+
+# %%
