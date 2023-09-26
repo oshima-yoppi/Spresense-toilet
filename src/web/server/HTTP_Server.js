ac12767ead948dd8dec50dca415187bea97d2eec
{neo-miyake} <miyake@kimura-lab.net>
2023-09-26 18:12:14 +0900

    Update HTTP_Server.js

--- src/web/server/HTTP_Server.js
+++ src/web/server/HTTP_Server.js
@@ -1,5 +1,52 @@
+// var http = require('http');
+// //var fs = require('fs');
+// var querystring = require('querystring');
+// var url = require('url');
+// var data;
+// var postData = {};
+// const hostname = '10.204.47.155';
+// const PORT = 3000;
+
+
+// var server = http.createServer(function(req, res) {
+// 	// Access '/', response back with the latest postData
+//     if (req.url === '/' && req.method === 'GET') {
+// 	console.log( 'GET Response :', postData['data']);
+// 	res.end( postData['data'] );
+//     }
+	
+//     // Access '/postPage' by POST method
+//     else if (req.url === '/postData' && req.method === 'POST') {
+// 	data = '';
+// 	req.on('data', function(chunk) {
+// 	    data += chunk;
+// 	});
+	
+// 	req.on('end', function() {
+// 	    // Parse the query
+// 	    postData =  querystring.parse(data);
+// 	    for( key in postData ){
+// 		// check the key
+// 		if( key == 'data' ){
+// 		    console.log( 'POST data    :', postData[key] );
+// 		    res.end(postData[key]);
+// 		}
+// 	    }
+// 	});
+//     }
+//     else {
+// 	res.statusCode = 404;
+// 	res.end('NotFound');
+//     }
+// });
+
+// // Listen port 10080
+// server.listen(PORT, () => {
+//     console.log(`Server running at http://${hostname}:${PORT}/`);
+//   });
+
 var http = require('http');
-//var fs = require('fs');
+var fs = require('fs'); // fsモジュールを追加
 var querystring = require('querystring');
 var url = require('url');
 var data;
@@ -7,40 +54,39 @@ var postData = {};
 const hostname = '10.204.47.155';
 const PORT = 3000;
 
-
 var server = http.createServer(function(req, res) {
-	// Access '/', response back with the latest postData
     if (req.url === '/' && req.method === 'GET') {
-	console.log( 'GET Response :', postData['data']);
-	res.end( postData['data'] );
-    }
-	
-    // Access '/postPage' by POST method
-    else if (req.url === '/postData' && req.method === 'POST') {
-	data = '';
-	req.on('data', function(chunk) {
-	    data += chunk;
-	});
-	
-	req.on('end', function() {
-	    // Parse the query
-	    postData =  querystring.parse(data);
-	    for( key in postData ){
-		// check the key
-		if( key == 'data' ){
-		    console.log( 'POST data    :', postData[key] );
-		    res.end(postData[key]);
-		}
-	    }
-	});
-    }
-    else {
-	res.statusCode = 404;
-	res.end('NotFound');
+        // ファイルを読み込み、レスポンスとして送信
+        fs.readFile('index.html', 'utf8', function(err, content) {
+            if (err) {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else if (req.url === '/postData' && req.method === 'POST') {
+        data = '';
+        req.on('data', function(chunk) {
+            data += chunk;
+        });
+
+        req.on('end', function() {
+            postData = querystring.parse(data);
+            for (key in postData) {
+                if (key == 'data') {
+                    console.log('POST data    :', postData[key]);
+                    res.end(postData[key]);
+                }
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.end('NotFound');
     }
 });
 
-// Listen port 10080
 server.listen(PORT, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
-  });
+});
