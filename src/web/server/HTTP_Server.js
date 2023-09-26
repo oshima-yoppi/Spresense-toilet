aa4fc6994dcb4c620521e3be9866783c435fbd48
{neo-miyake} <miyake@kimura-lab.net>
2023-09-26 18:53:18 +0900

    うわああああ

--- src/web/server/HTTP_Server.js
+++ src/web/server/HTTP_Server.js
@@ -9,36 +9,41 @@ const PORT = 3000;
 
 
 var server = http.createServer(function(req, res) {
+	// Access '/', response back with the latest postData
     if (req.url === '/' && req.method === 'GET') {
-        // ファイルを読み込んでクライアントに送信
-        fs.readFile('index.html', 'utf8', function(err, htmlContent) {
+		fs.readFile('index.html', 'utf8', function(err, content) {
             if (err) {
                 res.statusCode = 500;
                 res.end('Internal Server Error');
             } else {
-                res.setHeader('Content-Type', 'text/html');
-                res.end(htmlContent);
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                // res.end(content);
+				console.log( 'GET Response :', postData['data']);
+				res.end( postData['data'] );
             }
-        });
-    } else if (req.url === '/postData' && req.method === 'POST') {
-        data = '';
-        req.on('data', function(chunk) {
-            data += chunk;
         });
 
-        req.on('end', function() {
-            // Parse the query
-            postData = querystring.parse(data);
-            for (key in postData) {
-                // check the key
-                if (key == 'data') {
-                    console.log('POST data    :', postData[key]);
-                    res.setHeader('Content-Type', 'text/html');
-                    // 以下の行でpostData['data']の値をHTMLページに表示
-                    res.end(`POST Data: ${postData[key]}`);
-                }
-            }
-        });
+	
+    }
+	
+    // Access '/postPage' by POST method
+    else if (req.url === '/postData' && req.method === 'POST') {
+	data = '';
+	req.on('data', function(chunk) {
+	    data += chunk;
+	});
+	
+	req.on('end', function() {
+	    // Parse the query
+	    postData =  querystring.parse(data);
+	    for( key in postData ){
+		// check the key
+		if( key == 'data' ){
+		    console.log( 'POST data    :', postData[key] );
+		    res.end(postData[key]);
+		}
+	    }
+	});
     }
     else {
 	res.statusCode = 404;
