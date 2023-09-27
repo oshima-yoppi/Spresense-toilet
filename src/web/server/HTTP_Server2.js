6aa4a207bb42e5d1fded21775df344ec0ac4a380
{neo-miyake} <miyake@kimura-lab.net>
2023-09-27 18:47:24 +0900

    元に戻した

--- src/web/server/HTTP_Server2.js
+++ src/web/server/HTTP_Server2.js
@@ -1,45 +1,74 @@
-// サーバのコード
-
-const http = require('http');
-const fs = require('fs');
-const WebSocket = require('ws');
-
+var http = require('http');
+var fs = require('fs');
+var querystring = require('querystring');
+var url = require('url');
+var data;
+var postData = {};
 const hostname = '10.204.47.155';
 const PORT = 3000;
+let a = "loading..."
 
-let a = "loading...";
-
-// Create an HTTP server
-const server = http.createServer((req, res) => {
+var server = http.createServer(function(req, res) {
+    // Access '/', response back with the latest postData
     if (req.url === '/' && req.method === 'GET') {
-        fs.readFile('index.html', 'utf8', (err, content) => {
+        fs.readFile('index.html', 'utf8', function(err, content) {
             if (err) {
                 res.statusCode = 500;
                 res.end('Internal Server Error');
             } else {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
-                // Replace the placeholder with the current value of 'a'
-                content = content.replace('<span id="data_placeholder"></span>', a);
+
+                if ('data' in postData && postData['data'].trim() !== '') {
+                    // Embed postData['data'] into the HTML response
+                    console.log("postData['data']:", postData['data'])
+                    switch (postData['data']) {
+                        
+                        case '0':
+                            console.log("選択された値: 0")
+                            a = '非常に空いています';
+                            break;
+
+                        case '1':
+                            console.log("選択された値: 1")
+                            a = '空いています';
+                            break;
+
+                        case '2':
+                            console.log("選択された値: 2")
+                            a = '混雑しています';
+                            break;
+
+                        case '3':
+                            console.log("選択された値: 3")
+                            a = '非常に混雑しています';
+                            break;
+
+                        default:
+                            a = "loading..."
+                            console.log("error")
+                    }
+                    content = content.replace('<span id="data_placeholder"></span>', a);
+                } else {
+                    // If 'data' is empty, display "loading"
+                    content = content.replace('<span id="data_placeholder"></span>', ' loading...');
+                }
                 res.end(content);
             }
         });
     } else if (req.url === '/postData' && req.method === 'POST') {
-        let data = '';
-        req.on('data', (chunk) => {
+        data = '';
+        req.on('data', function(chunk) {
             data += chunk;
         });
 
-        req.on('end', () => {
-            const postData = new URLSearchParams(data);
-            for (const [key, value] of postData) {
-                if (key === 'data') {
-                    console.log('POST data:', value);
-                    a = determineStatus(value);
-                    // Send the updated value to connected WebSocket clients
-                    wsServer.clients.forEach((client) => {
-                        client.send(a);
-                    });
-                    res.end(value);
+        req.on('end', function() {
+            // Parse the query
+            postData = querystring.parse(data);
+            for (key in postData) {
+                // check the key
+                if (key == 'data') {
+                    console.log('POST data    :', postData[key]);
+                    res.end(postData[key]);
                 }
             }
         });
@@ -49,45 +78,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-// Create a WebSocket server
-const wsServer = new WebSocket.Server({ noServer: true });
-
-// Handle WebSocket connection
-wsServer.on('connection', (ws) => {
-    console.log('WebSocket connected');
-
-    // Send the initial value to the connected WebSocket client
-    ws.send(a);
-
-    ws.on('close', () => {
-        console.log('WebSocket disconnected');
-    });
-});
-
-// Upgrade the HTTP server to support WebSocket
-server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, (ws) => {
-        wsServer.emit('connection', ws, request);
-    });
-});
-
-// Listen on port 3000
-server.listen(PORT, hostname, () => {
+// Listen port 3000
+server.listen(PORT, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
 });
-
-// Function to determine the status based on the received value
-function determineStatus(value) {
-    switch (value) {
-        case '0':
-            return '非常に空いています';
-        case '1':
-            return '空いています';
-        case '2':
-            return '混雑しています';
-        case '3':
-            return '非常に混雑しています';
-        default:
-            return 'loading...';
-    }
-}
