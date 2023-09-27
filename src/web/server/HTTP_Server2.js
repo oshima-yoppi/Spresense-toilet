c9618ef6c9ecb07dbf5340c600900b3a5fbdd9d5
{neo-miyake} <miyake@kimura-lab.net>
2023-09-27 17:36:50 +0900

    path見る

--- src/web/server/HTTP_Server2.js
+++ src/web/server/HTTP_Server2.js
@@ -9,6 +9,8 @@ const PORT = 3000;
 let a = "loading..."
 
 var server = http.createServer(function(req, res) {
+    var fullPath = __dirname + filePath;
+    console.log('fullPath:', fullPath);
     // Access '/', response back with the latest postData
     if (req.url === '/' && req.method === 'GET') {
         fs.readFile('index.html', 'utf8', function(err, content) {
