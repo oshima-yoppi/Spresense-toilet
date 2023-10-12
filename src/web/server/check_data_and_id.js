c85c6acabf54ecc813e4f548991463021a8ba5ea
{neo-miyake} <miyake@kimura-lab.net>
2023-10-12 19:20:48 +0900

    人数だけ表示で変更

--- src/web/server/check_data_and_id.js
+++ src/web/server/check_data_and_id.js
@@ -1,18 +1,19 @@
 const chech_data_and_id = (value) => {
-    id = Math.floor(parseInt(value)%10);
+    // id = Math.floor(parseInt(value)%10);
     
-    if (Math.floor(parseInt(value)/10) === 0) {
-        displayValue = id + '非常に空いています';
-    } else if (Math.floor(parseInt(value)/10) === 1) {
-        displayValue = id + '空いています';
-    } else if (Math.floor(parseInt(value)/10) === 2) {
-        displayValue = id + '混雑しています';
-    } else if (Math.floor(parseInt(value)/10) === 3) {
-        displayValue = id + '非常に混雑しています';
-    } else {
-        displayValue = 'loading...';
-    }
-
+    // if (Math.floor(parseInt(value)/10) === 0) {
+    //     displayValue = id + '非常に空いています';
+    // } else if (Math.floor(parseInt(value)/10) === 1) {
+    //     displayValue = id + '空いています';
+    // } else if (Math.floor(parseInt(value)/10) === 2) {
+    //     displayValue = id + '混雑しています';
+    // } else if (Math.floor(parseInt(value)/10) === 3) {
+    //     displayValue = id + '非常に混雑しています';
+    // } else {
+    //     displayValue = 'loading...';
+    // }
+    id = 1;
+    displayValue = value;
     return displayValue;
   }
 
