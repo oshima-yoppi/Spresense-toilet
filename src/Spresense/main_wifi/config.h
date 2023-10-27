53922f47ac2588fc0cc66c4cd0973ca415cba6c3
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-10-27 12:15:57 +0900

    D

--- src/Spresense/main_wifi/config.h
+++ src/Spresense/main_wifi/config.h
@@ -1,18 +1,18 @@
 /*
  *  config.h - WiFi Configration Header
  *
- *  This work is free software; you can redistribute it and/or modify it under the terms 
- *  of the GNU Lesser General Public License as published by the Free Software Foundation; 
+ *  This work is free software; you can redistribute it and/or modify it under the terms
+ *  of the GNU Lesser General Public License as published by the Free Software Foundation;
  *  either version 2.1 of the License, or (at your option) any later version.
  *
- *  This work is distributed in the hope that it will be useful, but without any warranty; 
- *  without even the implied warranty of merchantability or fitness for a particular 
+ *  This work is distributed in the hope that it will be useful, but without any warranty;
+ *  without even the implied warranty of merchantability or fitness for a particular
  *  purpose. See the GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License along with 
- *  this work; if not, write to the Free Software Foundation, 
+ *  You should have received a copy of the GNU Lesser General Public License along with
+ *  this work; if not, write to the Free Software Foundation,
  *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*/
+ */
 
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
@@ -20,12 +20,17 @@
 /*-------------------------------------------------------------------------*
  * Configration
  *-------------------------------------------------------------------------*/
-#define  AP_SSID        "KimuraLab_StudentRoom"
-#define  PASSPHRASE     "since20070403"
+// #define  AP_SSID        "KimuraLab_StudentRoom"
+// #define  PASSPHRASE     "since20070403"
 
-#define  HTTP_SRVR_IP  "172.17.254.13"
-#define  HTTP_PORT     "3000"
-#define  HTTP_GET_PATH "/"
-#define  HTTP_POST_PATH "/postData"
+#define AP_SSID "neo_pixel"
+#define PASSPHRASE "neoneo1123"
+
+// #define  HTTP_SRVR_IP  "172.17.254.13"
+#define HTTP_SRVR_IP "192.168.27.164"
+
+#define HTTP_PORT "3000"
+#define HTTP_GET_PATH "/"
+#define HTTP_POST_PATH "/postData"
 
 #endif /*_CONFIG_H_*/
